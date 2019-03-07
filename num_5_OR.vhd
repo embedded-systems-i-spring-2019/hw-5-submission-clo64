c3178ae4e4b35f1b61ea293fc2a05ad8afc52c04
clo64 <47432133+clo64@users.noreply.github.com>
2019-03-07 16:26:52 -0500

    Update num_5_OR.vhd

--- num_5_OR.vhd
+++ num_5_OR.vhd
@@ -1,13 +1,13 @@
 --Charles Owen
 --Embbedded Systems
 --HW 5
--- Number 2 Case Statement
+-- Number 5 OR
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 
-entity num_2_case is
+entity num_5_OR is
 
     Port(
     
@@ -16,9 +16,9 @@ entity num_2_case is
     
     );
 
-end num_2_case;
+end num_5_OR;
 
-architecture Behavioral of num_2_case is
+architecture Behavioral of num_5_OR is
 
 begin
 
