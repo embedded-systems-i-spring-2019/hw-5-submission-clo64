9c2fc6c9d91a006dfeece839127a4e1725a0d0c4
clo64 <47432133+clo64@users.noreply.github.com>
2019-03-07 16:37:07 -0500

    Update num_6_decoder_case.vhd

--- num_6_decoder_case.vhd
+++ num_6_decoder_case.vhd
@@ -1,13 +1,13 @@
 --Charles Owen
 --Embbedded Systems
 --HW 5
--- Number 2 Case Statement
+-- Number 7 Decoder Case
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 
-entity num_2_case is
+entity num_7_case is
 
     Port(
     
@@ -16,9 +16,9 @@ entity num_2_case is
     
     );
 
-end num_2_case;
+end num_7_case;
 
-architecture Behavioral of num_2_case is
+architecture Behavioral of num_7_case is
 
 begin
 
